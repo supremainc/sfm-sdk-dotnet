fa82048028ea380f3709812e510fffd32cd61ebb
hyeonchang lee <hclee@suprema.co.kr>
2017-05-25 11:04:47 +0900

    Added library files. and modified project setting in pre-build event. Also, sample code is written.

---
+++ Sample/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Suprema.SFM_SDK_NET;
+
+namespace Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SFM_SDK_NET SFM = new SFM_SDK_NET();
+            UF_RET_CODE result = new UF_RET_CODE();
+            result = SFM.UF_InitCommPort("COM3", 19200, false);
+            Console.WriteLine(result.ToString());
+        }
+    }
+}
