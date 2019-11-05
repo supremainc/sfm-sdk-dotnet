a7fd85b92edd0f2d3712e5163ed3c138f9c45253
Brian Lee <hclee@suprema.co.kr>
2019-11-05 11:30:25 +0900

    WinForm example has been added.

---
+++ WinFormSample/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace WinFormSample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_connect = new System.Windows.Forms.Button();
+            this.btn_disconnect = new System.Windows.Forms.Button();
+            this.label_comPort = new System.Windows.Forms.Label();
+            this.label_baudrate = new System.Windows.Forms.Label();
+            this.tb_comPort = new System.Windows.Forms.TextBox();
+            this.tb_baudrate = new System.Windows.Forms.TextBox();
+            this.tb_message = new System.Windows.Forms.TextBox();
+            this.btn_enroll = new System.Windows.Forms.Button();
+            this.btn_identify = new System.Windows.Forms.Button();
+            this.btn_verify = new System.Windows.Forms.Button();
+            this.label_userID = new System.Windows.Forms.Label();
+            this.tb_userID = new System.Windows.Forms.TextBox();
+            this.btn_deleteTemplate = new System.Windows.Forms.Button();
+            this.btn_deleteAll = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btn_connect
+            // 
+            this.btn_connect.Location = new System.Drawing.Point(402, 45);
+            this.btn_connect.Name = "btn_connect";
+            this.btn_connect.Size = new System.Drawing.Size(75, 23);
+            this.btn_connect.TabIndex = 0;
+            this.btn_connect.Text = "connect";
+            this.btn_connect.UseVisualStyleBackColor = true;
+            this.btn_connect.Click += new System.EventHandler(this.btn_connect_Click);
+            // 
+            // btn_disconnect
+            // 
+            this.btn_disconnect.Location = new System.Drawing.Point(483, 45);
+            this.btn_disconnect.Name = "btn_disconnect";
+            this.btn_disconnect.Size = new System.Drawing.Size(75, 23);
+            this.btn_disconnect.TabIndex = 1;
+            this.btn_disconnect.Text = "disconnect";
+            this.btn_disconnect.UseVisualStyleBackColor = true;
+            this.btn_disconnect.Click += new System.EventHandler(this.btn_disconnect_Click);
+            // 
+            // label_comPort
+            // 
+            this.label_comPort.AutoSize = true;
+            this.label_comPort.Location = new System.Drawing.Point(12, 50);
+            this.label_comPort.Name = "label_comPort";
+            this.label_comPort.Size = new System.Drawing.Size(56, 13);
+            this.label_comPort.TabIndex = 2;
+            this.label_comPort.Text = "Com Port :";
+            // 
+            // label_baudrate
+            // 
+            this.label_baudrate.AutoSize = true;
+            this.label_baudrate.Location = new System.Drawing.Point(164, 50);
+            this.label_baudrate.Name = "label_baudrate";
+            this.label_baudrate.Size = new System.Drawing.Size(56, 13);
+            this.label_baudrate.TabIndex = 3;
+            this.label_baudrate.Text = "Baudrate :";
+            // 
+            // tb_comPort
+            // 
+            this.tb_comPort.Location = new System.Drawing.Point(71, 46);
+            this.tb_comPort.Name = "tb_comPort";
+            this.tb_comPort.Size = new System.Drawing.Size(74, 20);
+            this.tb_comPort.TabIndex = 4;
+            // 
+            // tb_baudrate
+            // 
+            this.tb_baudrate.Location = new System.Drawing.Point(224, 45);
+            this.tb_baudrate.Name = "tb_baudrate";
+            this.tb_baudrate.Size = new System.Drawing.Size(74, 20);
+            this.tb_baudrate.TabIndex = 5;
+            // 
+            // tb_message
+            // 
+            this.tb_message.Location = new System.Drawing.Point(15, 12);
+            this.tb_message.Name = "tb_message";
+            this.tb_message.ReadOnly = true;
+            this.tb_message.Size = new System.Drawing.Size(543, 20);
+            this.tb_message.TabIndex = 6;
+            this.tb_message.Text = "WinFormSample";
+            this.tb_message.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btn_enroll
+            // 
+            this.btn_enroll.Location = new System.Drawing.Point(20, 32);
+            this.btn_enroll.Name = "btn_enroll";
+            this.btn_enroll.Size = new System.Drawing.Size(122, 23);
+            this.btn_enroll.TabIndex = 7;
+            this.btn_enroll.Text = "Enroll";
+            this.btn_enroll.UseVisualStyleBackColor = true;
+            this.btn_enroll.Click += new System.EventHandler(this.btn_enroll_Click);
+            // 
+            // btn_identify
+            // 
+            this.btn_identify.Location = new System.Drawing.Point(148, 32);
+            this.btn_identify.Name = "btn_identify";
+            this.btn_identify.Size = new System.Drawing.Size(122, 23);
+            this.btn_identify.TabIndex = 8;
+            this.btn_identify.Text = "Identify";
+            this.btn_identify.UseVisualStyleBackColor = true;
+            this.btn_identify.Click += new System.EventHandler(this.btn_identify_Click);
+            // 
+            // btn_verify
+            // 
+            this.btn_verify.Location = new System.Drawing.Point(276, 32);
+            this.btn_verify.Name = "btn_verify";
+            this.btn_verify.Size = new System.Drawing.Size(122, 23);
+            this.btn_verify.TabIndex = 9;
+            this.btn_verify.Text = "Verify";
+            this.btn_verify.UseVisualStyleBackColor = true;
+            this.btn_verify.Click += new System.EventHandler(this.btn_verify_Click);
+            // 
+            // label_userID
+            // 
+            this.label_userID.AutoSize = true;
+            this.label_userID.Location = new System.Drawing.Point(12, 91);
+            this.label_userID.Name = "label_userID";
+            this.label_userID.Size = new System.Drawing.Size(49, 13);
+            this.label_userID.TabIndex = 10;
+            this.label_userID.Text = "User ID :";
+            // 
+            // tb_userID
+            // 
+            this.tb_userID.Location = new System.Drawing.Point(71, 88);
+            this.tb_userID.Name = "tb_userID";
+            this.tb_userID.Size = new System.Drawing.Size(74, 20);
+            this.tb_userID.TabIndex = 11;
+            // 
+            // btn_deleteTemplate
+            // 
+            this.btn_deleteTemplate.Location = new System.Drawing.Point(404, 32);
+            this.btn_deleteTemplate.Name = "btn_deleteTemplate";
+            this.btn_deleteTemplate.Size = new System.Drawing.Size(122, 23);
+            this.btn_deleteTemplate.TabIndex = 12;
+            this.btn_deleteTemplate.Text = "Delete Template";
+            this.btn_deleteTemplate.UseVisualStyleBackColor = true;
+            this.btn_deleteTemplate.Click += new System.EventHandler(this.btn_deleteTemplate_Click);
+            // 
+            // btn_deleteAll
+            // 
+            this.btn_deleteAll.Location = new System.Drawing.Point(20, 61);
+            this.btn_deleteAll.Name = "btn_deleteAll";
+            this.btn_deleteAll.Size = new System.Drawing.Size(122, 23);
+            this.btn_deleteAll.TabIndex = 13;
+            this.btn_deleteAll.Text = "Delete All";
+            this.btn_deleteAll.UseVisualStyleBackColor = true;
+            this.btn_deleteAll.Click += new System.EventHandler(this.btn_deleteAll_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btn_enroll);
+            this.groupBox1.Controls.Add(this.btn_deleteAll);
+            this.groupBox1.Controls.Add(this.btn_identify);
+            this.groupBox1.Controls.Add(this.btn_deleteTemplate);
+            this.groupBox1.Controls.Add(this.btn_verify);
+            this.groupBox1.Location = new System.Drawing.Point(15, 133);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(548, 102);
+            this.groupBox1.TabIndex = 14;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Basic function";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(575, 469);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.tb_userID);
+            this.Controls.Add(this.label_userID);
+            this.Controls.Add(this.tb_message);
+            this.Controls.Add(this.tb_baudrate);
+            this.Controls.Add(this.tb_comPort);
+            this.Controls.Add(this.label_baudrate);
+            this.Controls.Add(this.label_comPort);
+            this.Controls.Add(this.btn_disconnect);
+            this.Controls.Add(this.btn_connect);
+            this.Name = "Form1";
+            this.Text = "WinFormSample";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_connect;
+        private System.Windows.Forms.Button btn_disconnect;
+        private System.Windows.Forms.Label label_comPort;
+        private System.Windows.Forms.Label label_baudrate;
+        private System.Windows.Forms.TextBox tb_comPort;
+        private System.Windows.Forms.TextBox tb_baudrate;
+        private System.Windows.Forms.TextBox tb_message;
+        private System.Windows.Forms.Button btn_enroll;
+        private System.Windows.Forms.Button btn_identify;
+        private System.Windows.Forms.Button btn_verify;
+        private System.Windows.Forms.Label label_userID;
+        private System.Windows.Forms.TextBox tb_userID;
+        private System.Windows.Forms.Button btn_deleteTemplate;
+        private System.Windows.Forms.Button btn_deleteAll;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ WinFormSample/Form1.cs
@@ -0,0 +1,268 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Suprema.SFM_SDK_NET;
+
+namespace WinFormSample
+{
+    public partial class Form1 : Form
+    {
+        public SFM_SDK_NET sdk = new SFM_SDK_NET();
+        public UF_RET_CODE ret = new UF_RET_CODE();
+        public EnrollCallback enrollCallback;
+        public IdentifyCallback identifyCallback;
+        public VerifyCallback verifyCallback;
+        public String m_ComPort;
+        public int m_Baudrate = 115200;
+        public bool m_AsciiMode = false;
+        public bool m_bConnected = false;
+        public uint m_userID = 0;
+                               
+        public Form1()
+        {
+            InitializeComponent();
+            tb_comPort.Text = "COM8";
+            tb_baudrate.Text = String.Format("{0}", m_Baudrate);
+            tb_userID.Text = String.Format("{0}", m_userID);
+
+            enrollCallback = new EnrollCallback(EnrollCallbackFunction);
+            identifyCallback = new IdentifyCallback(ScanCallback);
+            verifyCallback = new VerifyCallback(ScanCallback);
+            
+        }
+         
+        public void EnrollCallbackFunction(byte errCode, UF_ENROLL_MODE enrollMode, int numOfSuccess)
+        {
+            if (errCode == (byte)UF_PROTOCOL_RET_CODE.UF_PROTO_RET_SCAN_SUCCESS)
+            {
+                Message("Scan Success");
+            }
+            else if(errCode == (byte)UF_PROTOCOL_RET_CODE.UF_PROTO_RET_SUCCESS && numOfSuccess == 1)
+            {
+                Message("Place your finger on the sensor again!");
+            }
+            else
+            {
+                Message(String.Format("Error : {0:X}", errCode));
+            }            
+        }
+        void ScanCallback(byte errCode)
+        {
+            if(errCode == (byte)UF_PROTOCOL_RET_CODE.UF_PROTO_RET_SCAN_SUCCESS)
+            {
+                Message("Scan Success");                
+            }
+            else
+            {
+                Message(String.Format("Error : {0:X}", errCode));
+            }            
+        }
+
+        private void Message(String message)
+        {
+            tb_message.Text = message;
+            Application.DoEvents();
+        }
+
+        private void Connect()
+        {
+            if (m_bConnected == true)
+            {
+                Disconnect();
+            }
+
+            m_ComPort = tb_comPort.Text;
+            m_Baudrate = Convert.ToInt32(tb_baudrate.Text);
+            m_AsciiMode = false; //defualt
+
+            ret = sdk.UF_InitCommPort(m_ComPort, m_Baudrate, m_AsciiMode);
+            
+            if (ret != UF_RET_CODE.UF_RET_SUCCESS)
+            {
+                Message(String.Format("Error : {0} , Failed to initialize COM Port", ret.ToString()));
+                return;
+            }
+
+            uint param = 0;
+            uint size = 0;
+            byte flag = 0;
+            ret = sdk.UF_Command((byte)UF_COMMAND.UF_COM_SS, ref param, ref size, ref flag);
+
+            if(ret != UF_RET_CODE.UF_RET_SUCCESS)
+            {
+                Message(String.Format("Error : {0}, Failed to connect", ret.ToString()));
+                return;
+            }
+
+
+            sdk.UF_InitSysParameter();
+            sdk.UF_SetEnrollCallback(enrollCallback);
+            sdk.UF_SetIdentifyCallback(identifyCallback);
+            sdk.UF_SetVerifyCallback(verifyCallback);
+            
+
+            Message("Connected");
+
+            m_bConnected = true;
+
+        }
+
+        private void Disconnect()
+        {
+            if(m_bConnected)
+            {
+                ret = sdk.UF_CloseCommPort();
+                if (ret != UF_RET_CODE.UF_RET_SUCCESS)
+                {
+                    Message(String.Format("Error : {0} , Failed to close COM port", ret.ToString()));
+                }
+                Message("Disconnected");
+                m_bConnected = false;
+            }
+        }
+
+        private void btn_connect_Click(object sender, EventArgs e)
+        {
+            Connect();
+        }
+
+        private void btn_disconnect_Click(object sender, EventArgs e)
+        {
+            Disconnect();
+        }
+
+        private void btn_enroll_Click(object sender, EventArgs e)
+        {
+            if (!m_bConnected)
+            {
+                Message("Module is not connected");
+                return;
+            }
+
+            if (tb_userID.Text == "")
+                tb_userID.Text = "0";
+
+            m_userID = Convert.ToUInt32(tb_userID.Text);
+
+            uint enrolledID = 0;
+            uint imageQuality = 0;
+
+            Message("Place your finger on the sensor");
+            ret = sdk.UF_Enroll(m_userID, UF_ENROLL_OPTION.UF_ENROLL_AUTO_ID, ref enrolledID, ref imageQuality);
+            
+            if(ret == UF_RET_CODE.UF_RET_SUCCESS)
+            {
+                Message(String.Format("User {0} has been enrolled ({1})", enrolledID, imageQuality));
+                tb_userID.Text = String.Format("{0}", enrolledID);
+                return;
+            }
+
+            Message(String.Format("Error : {0} , Failed to enroll", ret.ToString()));
+      
+        }
+
+        private void btn_identify_Click(object sender, EventArgs e)
+        {
+            if (!m_bConnected)
+            {
+                Message("Module is not connected");
+                return;
+            }
+
+            uint userID = 0;
+            byte subID = 0;
+
+            Message("Place your finger on the sensor");
+            ret = sdk.UF_Identify(ref userID, ref subID);
+
+            if (ret == UF_RET_CODE.UF_RET_SUCCESS)
+            {
+                Message(String.Format("Identify Success, User ID : {0} ({1})", userID, subID));
+                return;
+            }
+
+            Message(String.Format("Error : {0} , Failed to identify", ret.ToString()));
+        }
+
+        private void btn_verify_Click(object sender, EventArgs e)
+        {
+            if (!m_bConnected)
+            {
+                Message("Module is not connected");
+                return;
+            }
+
+            uint userID = Convert.ToUInt32(tb_userID.Text);
+            byte subID = 0;
+
+            if (userID == 0)
+            {
+                Message("User ID is not entered");
+                return;
+            }
+            
+            Message("Place your finger on the sensor");
+            ret = sdk.UF_Verify(userID, ref subID);
+            
+            if (ret == UF_RET_CODE.UF_RET_SUCCESS)
+            {
+                Message(String.Format("Verify Success, User ID : {0} ({1})", userID, subID));
+                return;
+            }
+
+            Message(String.Format("Error : {0} , Failed to identify", ret.ToString()));
+        }
+
+        private void btn_deleteTemplate_Click(object sender, EventArgs e)
+        {
+            if (!m_bConnected)
+            {
+                Message("Module is not connected");
+                return;
+            }
+          
+            uint userID = Convert.ToUInt32(tb_userID.Text);
+
+            if (userID == 0)
+            {
+                Message("User ID is not entered");
+                return;
+            }
+
+            ret = sdk.UF_Delete(userID);
+
+            if (ret == UF_RET_CODE.UF_RET_SUCCESS)
+            {
+                Message(String.Format("Delete Success"));
+                return;
+            }
+
+            Message(String.Format("Error : {0} , Failed to delete the template", ret.ToString()));
+        }
+
+        private void btn_deleteAll_Click(object sender, EventArgs e)
+        {
+            if (!m_bConnected)
+            {
+                Message("Module is not connected");
+                return;
+            }
+
+            ret = sdk.UF_DeleteAll();
+
+            if (ret == UF_RET_CODE.UF_RET_SUCCESS)
+            {
+                Message(String.Format("All templates has been deleted"));
+                return;
+            }
+
+            Message(String.Format("Error : {0} , Failed to delete all templates", ret.ToString()));
+        }
+    }
+}
